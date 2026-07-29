ee7d0ab367b257dfa13a8a9c37d7544ddf4a269d
nakacy0919-Git <nakacy0919@gmail.com>
2026-07-29 10:44:28 +0900

    minor修正

--- src/pages/TeacherView.jsx
+++ src/pages/TeacherView.jsx
@@ -126,7 +126,7 @@ export default function TeacherView() {
                 )}
                 
                 <Link to="/" className="px-5 py-2.5 bg-slate-100 text-slate-600 font-bold rounded-xl hover:bg-slate-200 transition-all ml-2" title="トップ画面（設定）に戻ります">
-                  トップ(新規作成)
+                  TOP画面へ
                 </Link>
               </div>
             </div>
